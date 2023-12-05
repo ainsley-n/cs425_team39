97e9ca326a20916d0d38dcd93b1d037dc2225fd5
ainsley nutting <a.nutting@nevada.unr.edu>
2023-12-05 09:43:37 -0800

    fixed mesh for now

--- mesh_analysis.py
+++ mesh_analysis.py
@@ -1,23 +1,23 @@
 from lcapy import Circuit
+from sympy import sympify
 
-# def perform_mesh_analysis(circuit):
-#     #time domain
-#     l = circuit.mesh_analysis()
-#     mesh_equations = l.mesh_equations()
+#this was from the lcapy documentation
+def perform_lcapy_mesh(circuit):
+    # mesh equations using LaPlace Domain
+    l = circuit.laplace().mesh_analysis()
+    laplace_mesh_equations = l.mesh_equations()
 
-#     print("Mesh Equations (Time Domain):")
-#     #print equations line by line
-#     for variable, equation in mesh_equations.items():
-#         print(f"{variable}: {equation}")
-
-#     #LaPlace Domain
-#     l = circuit.laplace().mesh_analysis()
-#     laplace_mesh_equations = l.mesh_equations()
-
-#     print("\nMesh Equations (Laplace Domain):")
-#     #print Laplace domain equations line by line
-#     for variable, equation in laplace_mesh_equations.items():
-#         print(f"{variable}: {equation}")
+    print("\nMesh Equations :")
+    #print Laplace domain equations line by line
+    for variable, equation in laplace_mesh_equations.items():
+        print(f"{variable}: {sympify(str(equation))}")
+        
+    # laplace_matrix_equations = l.matrix_equations(form='A y = b')
+    # print(laplace_matrix_equations)
+    
+    # laplace_solutions = l.solve_laplace()
+    # print('SOLUTIONSS')
+    # print(laplace_solutions)
 
 def parse_netlist(netlist):
     components = []
@@ -59,21 +59,25 @@ def organize_components(components):
     for component_type, nodes in components:
         if component_type != 'W':  # Exclude 'W' components
             for node in nodes:
-                if node not in node_dict:
-                    node_dict[node] = []
-                node_dict[node].append((component_type, nodes))
+                normalized_node = str(node).split('_')[0] if '_' in str(node) else str(node)
+                if normalized_node not in node_dict:
+                    node_dict[normalized_node] = []
+                node_dict[normalized_node].append((component_type, nodes))
 
     return node_dict
 
 def sort_by_node(organized_components):
-    sorted_components_list = []
+    sorted_components_dict = {}
     for node, components in organized_components.items():
+        sorted_components_list = []
         for component_type, nodes in components:
             # Handle both integer and string nodes
             normalized_nodes = [str(node).split('_')[0] if '_' in str(node) else str(node) for node in nodes]
             sorted_components_list.append({component_type: normalized_nodes})
-        
-    return sorted_components_list
+        sorted_components_dict[node] = sorted_components_list
+
+    return sorted_components_dict
+
 
 
 
@@ -82,8 +86,16 @@ def perform_mesh_analysis(circuit):
     # Parse and organize components
     components = parse_netlist(circuit)
     organized_components = organize_components(components)
-    print(f'Organized Components {organized_components}')
+    print(f'Organized Components {organized_components}\n')
 
     # Print organized components
     sorted_components = sort_by_node(organized_components)
-    print(f'Sorted Components {sorted_components}')
+    print(f'Sorted Components')
+    for node, components_list in sorted_components.items():
+        print(f"{node}:")
+        for component_dict in components_list:
+            for component_type, nodes in component_dict.items():
+                print(f"  {component_type} {', '.join(map(str, nodes))}")
+        print()
+        
+    perform_lcapy_mesh(circuit)
